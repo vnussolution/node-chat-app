bda43b83bf29be8dee0cd78798a5b8709bd44739
frank <vnussolution@gmail.com>
2017-07-03 21:32:38 -0700

    hook up index.html

--- server/server.js
+++ server/server.js
@@ -2,7 +2,28 @@
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 
 const PORT = process.env.PORT || 3000;
 
+var app = express();
+
+app.use(express.static(publicPath));
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} : ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) console.log('Failed to appendFile::', err);
+    });
+    next();
+
+});
+
+
+app.get('/', (req, res) => {
+    res.render('index.html');
+})
+
+app.listen(PORT, () => console.log(`server is up on port: ${PORT}`));
